76973f51ad7966b39b290c7f5387664f32945065
simbankenya <pharesmuruthi@gmail.com>
2023-05-10 14:31:42 +0300

    set up order and order-items schema

--- models/order.js
+++ models/order.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    
 });
 
 
